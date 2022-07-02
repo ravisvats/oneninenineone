45811c83b252d5d541beb151d8713d84cc1cdd0f
ravisvats <ravisvats@gmail.com>
2022-07-02 18:27:38 +0530

    authetication

--- ablog/settings.py
+++ ablog/settings.py
@@ -26,7 +26,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'theblog'
+    'theblog',
+    'members',
 ]
 
 MIDDLEWARE = [
@@ -116,8 +117,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
